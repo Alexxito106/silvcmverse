2a91a219a82bb3d0826d8fa42a44c97987556360
AlexFreire <alex.freirebarriga24@campusfp.es>
2026-03-17 12:50:42 +0100

    third try to fix build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname
+  }
+}
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
-
-
-
+module.exports = nextConfig
